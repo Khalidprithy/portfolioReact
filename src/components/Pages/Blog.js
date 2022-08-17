7d433d23073c4dcef942a9ff5a21923451d25e6c
Khalidprithy <khalidprithy@gmail.com>
2022-08-18 02:38:23 +0600

    blog updated

--- src/components/Pages/Blog.js
+++ src/components/Pages/Blog.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div className='flex flex-col items-center justify-center min-h-screen bg-primary'>
-            <h4 className="text-5xl text-neutral font-bold mb-6">Blog</h4>
-            <h4 className='text-4xl text-neutral'>Coming Soon</h4>
+        <div>
+
         </div>
     );
 };
